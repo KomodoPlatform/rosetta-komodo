ea2877b869316521596ad4bf2214dd1ae3acefea
DeckerSU <deckersu@protonmail.com>
2024-03-20 17:49:19 +0100

    update latest supported transaction version

--- komodod/wire/msgtx.go
+++ komodod/wire/msgtx.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	// TxVersion is the current latest supported transaction version.
-	TxVersion = 1
+	TxVersion = 4
 
 	// MaxTxInSequenceNum is the maximum sequence number the sequence field
 	// of a transaction input can be.
